679a449f88ba05939ea3954ea860dbb168acd1c0
xh3t4g <xh3t4g@gmail.com>
2026-03-30 13:08:26 +0300

    Добавил express.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const app = express();
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(4040, () => {
+    console.log('Успешный запуск http://localhost:4040')
+});
